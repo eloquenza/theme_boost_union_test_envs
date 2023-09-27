47e04891b55bc730780479ea17c0d09e8060cb76
Dennis Grabowski <dennis.grabowski@hs-hannover.de>
2023-09-27 16:49:34 +0200

    fix(config): return absolute paths
    Just safer to work with absolute paths instead of relative ones.

--- theme_boost_union_test_envs/utils/config.py
+++ theme_boost_union_test_envs/utils/config.py
@@ -1,3 +1,4 @@
+import os
 from typing import Any
 
 from dependency_injector import containers, providers
@@ -8,5 +9,5 @@ APP_KEY = "core"
 class ApplicationConfigManager:
     def __init__(self, config: dict[Any, Any]) -> None:
         # for some reason, the passed argument 'config' isn't of type providers.Configuration anymore, which makes saving it and accessing all elements with the dot notation not possible
-        self.working_dir = config[APP_KEY]["working_dir"]
-        self.moodle_cache_dir = config[APP_KEY]["moodle_cache_dir"]
+        self.working_dir = os.path.abspath(config[APP_KEY]["working_dir"])
+        self.moodle_cache_dir = os.path.abspath(config[APP_KEY]["moodle_cache_dir"])
